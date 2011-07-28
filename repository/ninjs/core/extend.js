818cd7016c2293abf22d813d4b59dc4df280634e
Dayton Nolan <daytonn@gmail.com>
2011-07-28 09:53:33 -0500

    Fix unless logic

--- repository/ninjs/core/extend.js
+++ repository/ninjs/core/extend.js
@@ -17,15 +17,11 @@
 
 	if (is_undefined(window.unless)) {
 		window.unless = function(expression, callback, fallback) {
-			if (is_undefined(expression)) {
-				throw new SyntaxError("unless(expression, callback[, fallback]): expression is undefined");
-			}
-
 			if (is_undefined(callback)) {
 				throw new SyntaxError("unless(expression, callback[, fallback]): callback is undefined");
 			}
 
-			if (!expression) {
+			if (!!!expression) {
 				callback.call(this);
 			}
 			else if (is_defined(fallback)) {
